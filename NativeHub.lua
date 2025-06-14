0f0052ab15cfaa499edfb59f83500f8406e61507
Native-Scripts <ajgapo639@gmail.com>
2025-06-15 03:13:34 +0800

    Update NativeHub.lua

--- NativeHub.lua
+++ NativeHub.lua
@@ -1,1 +1,3 @@
-loadstring(game:HttpGet("https://paste.ee/r/zy8djk90"))()
+local e="aHR0cHM6Ly9wYXN0ZS5lZS9yL3p5OGRqazkw"
+local d=function(s)local b='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'s=string.gsub(s,'[^'..b..'=]','')return(s:gsub('.',function(x)if x=='='then return''end;local r,f='',(b:find(x)-1)for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and'1'or'0')end;return r end):gsub('%d%d%d?%d?%d?%d?%d?%d?',function(x)if#x~=8 then return''end;local c=0;for i=1,8 do c=c+(x:sub(i,i)=='1'and 2^(8-i)or 0)end;return string.char(c)end))end;
+loadstring(game:HttpGet(d(e)))()
